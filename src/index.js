a187338081997c4d29f1d0e63dbbd34b004be17d
RgMellon <renan_gian@hotmail.com>
2024-12-30 08:43:30 -0300

    feat: add error handling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = require('./router');
+require('express-async-errors');
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use((error, request, response, next) => {
+  console.log('####error', error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => {
   console.log('Online');
